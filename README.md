a0da672ff9f63d4f05a82a3aed7ea067fc01fe25
Satya Kalyan <satyakalyan5@gmail.com>
2024-11-08 11:01:42 -0700

    Renamed tests folder

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # seleniumJS
+
+* Reference Video: <https://www.youtube.com/watch?v=BQ-9e13kJ58&list=PLZMWkkQEwOPl0udc9Dap2NbEAkwkdOTV3&index=1>
+* Reference document: <https://www.selenium.dev/documentation/webdriver/elements/>
---
+++ test/firstTest.js
@@ -0,0 +1,41 @@
+// const { Builder, By, Key } = require("selenium-webdriver");
+// const assert = require("assert");
+
+import { Builder, By, Key } from "selenium-webdriver";
+import assert from "assert";
+import { expect } from "chai";
+
+//describe block
+describe("add todo tests", function () {
+	//it block
+	it("successfully adds todo", async function () {
+		//launch the browser after building a new Chrome instance
+		let driver = await new Builder().forBrowser("chrome").build();
+
+		//navigate to application
+		await driver.get("https://lambdatest.github.io/sample-todo-app/");
+
+		//add a todo
+		let currToDo = "Build Selenium Framework";
+		await driver
+			.findElement(By.id("sampletodotext"))
+			.sendKeys(currToDo, Key.RETURN);
+
+		//assert
+		let todoText = await driver
+			.findElement(By.xpath("//li[last()]"))
+			.getText()
+			.then(function (val) {
+				return val;
+			});
+
+		//assertion using node
+		// assert.strictEqual(todoText, currToDo);
+
+		//chai assertion
+		expect(todoText).to.equal(currToDo);
+
+		//close the browser
+		await driver.quit();
+	});
+});

f35439d3d1de93dd68aa6918ea491a33c55ff28f
Satya Kalyan <satyakalyan5@gmail.com>
2024-11-07 20:58:37 -0700

    Added basic assertions

--- tests/firstTest.js
+++ tests/firstTest.js
@@ -1,16 +1,29 @@
 const { Builder, By, Key } = require("selenium-webdriver");
+const assert = require("assert");
 
 async function example() {
-	//launch the browser
+	//launch the browser after building a new Chrome instance
 	let driver = await new Builder().forBrowser("chrome").build();
 
 	//navigate to application
 	await driver.get("https://lambdatest.github.io/sample-todo-app/");
 
 	//add a todo
+	let currToDo = "Build Selenium Framework";
 	await driver
 		.findElement(By.id("sampletodotext"))
-		.sendKeys("Learn Selenium with JS", Key.RETURN);
+		.sendKeys(currToDo, Key.RETURN);
+
+	//assert
+	let todoText = await driver
+		.findElement(By.xpath("//li[last()]"))
+		.getText()
+		.then(function (val) {
+			return val;
+		});
+
+	//assertion
+	assert.strictEqual(todoText, "slkndfglj");
 
 	//close the browser
 	await driver.quit();
